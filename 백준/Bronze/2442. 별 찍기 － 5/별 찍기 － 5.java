ae15cd65dee896bf02716a8f55172eae875e0cfc
Sara <rha4578@gmail.com>
2022-09-04 00:06:42 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 216 ms, Memory: 18708 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N; j++) {
+				if(j<N-i-1) {System.out.print(" ");}
+				else {System.out.print("*");}}
+			for(int j=0; j<N-1; j++) {
+				if(i-1>=j)System.out.print("*");
+			}
+			if(i!=N-1)System.out.println();
+		}
+
+	}
+
+}
