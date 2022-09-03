4a899d6c2d8e4e65e0d30e189154d87db97acc6b
Sara <rha4578@gmail.com>
2022-09-04 00:41:04 +0900

    [Bronze IV] Title: 연세대학교, Time: 100 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		sc.close();
+		if(N==1) {
+			System.out.println("Leading the Way to the Future");
+		}
+		else {
+			System.out.println("YONSEI");
+		}
+				
+
+	}
+
+}
