0b03cbd80cf30aa1993a88abb38fa7cc663b4389
Sara <rha4578@gmail.com>
2022-09-04 00:43:28 +0900

    [Bronze V] Title: 홍익대학교, Time: 208 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		System.out.println(N-1946);
+
+	}
+
+}
