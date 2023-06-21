d494e6e182a26b61eae1daea00af01f6533724dc
Sara <rha4578@gmail.com>
2023-06-21 23:25:45 +0900

    [Bronze V] Title: 꼬마 정민, Time: 124 ms, Memory: 14100 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        Long A = Long.parseLong(st.nextToken());
+        Long B = Long.parseLong(st.nextToken());
+        Long C = Long.parseLong(st.nextToken());
+
+        System.out.println(A + B + C);
+    }
+}
