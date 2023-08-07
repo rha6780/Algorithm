6bda100875df60b240e541927813e9e459898e50
Sara <rha4578@gmail.com>
2023-08-07 20:32:34 +0900

    [Silver IV] Title: 차집합, Time: 4356 ms, Memory: 255764 KB -BaekjoonHub

--- 백준/Silver/1822. 차집합/차집합.java
+++ 백준/Silver/1822. 차집합/차집합.java
@@ -1,33 +1,38 @@
-	import java.util.HashSet;
-import java.util.ArrayList;
-import java.util.Arrays;
-	import java.util.Collection;
-import java.util.Collections;
-import java.util.Set; 
-	import java.util.Scanner;
-	import java.io.*;
-	public class Main {
+import java.util.*;
+import java.io.*;
 
-	public static void main(String[] args) throws IOException {
-	    HashSet<Integer> S=new HashSet<Integer>();
-	    HashSet<Integer> T=new HashSet<Integer>();
-	    Scanner sc=new Scanner(System.in);
-	    int NA=sc.nextInt(); 
-	    int NB=sc.nextInt();
-	    for(int i=0; i<NA; i++) {
-	        S.add(sc.nextInt());
-	    }
-	    for(int i=0; i<NB; i++) {
-	        T.add(sc.nextInt());
-	    }sc.close();
-	    S.removeAll(T);
-	    ArrayList U=new ArrayList(S);
-	    Collections.sort(U);
-	    if(0==S.size()) System.out.print("0");
-	    else {
-	    System.out.println(S.size());
-	   for(Object s:U) {
-		   System.out.print(s+" ");}
-	    }
-	}
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        HashSet<Integer> S = new HashSet<Integer>();
+        HashSet<Integer> T = new HashSet<Integer>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int NA = Integer.parseInt(st.nextToken());
+        int NB = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < NA; i++) {
+            S.add(Integer.parseInt(st.nextToken()));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < NB; i++) {
+            T.add(Integer.parseInt(st.nextToken()));
+        }
+
+        S.removeAll(T);
+
+        ArrayList<Integer> U = new ArrayList<Integer>(S);
+        Collections.sort(U);
+
+        if (0 == S.size()) {
+            System.out.print("0");
+        } else {
+            System.out.println(S.size());
+            for (Object s : U) {
+                System.out.print(s + " ");
+            }
+        }
+    }
 }
