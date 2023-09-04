b73b32c0e458411dce823cf49ad904530646e0aa
Sara <rha4578@gmail.com>
2023-09-04 23:27:56 +0900

    [Bronze V] Title: 문자열, Time: 112 ms, Memory: 13976 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<N; i++) {
+			String str = br.readLine();
+
+			sb.append(str.charAt(0));
+			sb.append(str.charAt(str.length()-1));
+			sb.append("\n");
+		}
+
+		System.out.println(sb);
+	}
+
+}
